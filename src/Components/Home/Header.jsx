7a0472d68b4c7d16c5f24c19df3e58c7cae86442
CLINTON <clintonchiemeka@gmail.com>
2024-09-16 17:22:25 +0100

    User can view a list of all articles

---
+++ src/Components/Home/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <nav className="flex items-center justify-between bg-gray-300 p-4">
+        <button type="button" className="bg-gray-200">Menu</button>
+        <input type="button" className="text-white hover:bg-blue-700 px-3 py-2 rounded" value="search" />
+        <button type="button" className="bg-gray-200">Home</button>
+        <button type="button" className="bg-gray-200">Search</button>
+        <button type="button" className="bg-gray-200">Articles</button>
+        <button type="button" className="bg-gray-200">Profile</button>
+        <button type="button" className="bg-gray-200">Settings</button>
+    </nav>
+  );
+};
+
+export default Header;
